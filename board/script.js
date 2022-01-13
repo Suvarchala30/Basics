14581bc86ebc0885db7e1d630d32eaec2ceb111b
Suvarchala30 <suvarchalareddy1998@gmail.com>
2022-01-13 17:03:27 +0530

    13

--- board/script.js
+++ board/script.js
@@ -1,1 +1,33 @@
-const colors=[]
+const colors=['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71']
+const container=document.getElementById("container")
+
+const Squares=800
+for(let i=0;i<Squares;i++){
+    const square=document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener("mouseover",()=>{
+        setcolor(square)
+    })
+    square.addEventListener("mouseout",()=>{
+        removecolor(square)
+    })
+    container.appendChild(square)
+}
+
+
+function setcolor(element){
+    const color=randomcolor()
+    element.style.background=color
+    element.style.boxShadow= `0 0 2px $color, 0 0 10px $color`
+
+}
+
+function removecolor(element){
+    element.style.background="#1d1d1d"
+    element.style.boxShadow="0 0 2px #000"
+}
+function randomcolor(){
+    const index=Math.floor(Math.random()*colors.length)
+    return colors[index]
+}
