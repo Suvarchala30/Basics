10a55ac1d9240205317a1c78c86d638fb7473ec1
Suvarchala30 <suvarchalareddy1998@gmail.com>
2022-01-19 21:31:42 +0530

    78

--- jan18-class/callapplybind.js
+++ jan18-class/callapplybind.js
@@ -7,6 +7,15 @@ functions
 -code, name [optional](properties)
 -call,apply,bind (methods)
 */
-function fun1(){
-    console.log('abc')
-}
+// function fun1(){
+//     console.log('abc')
+// }
+
+
+
+// function abc(){
+//     console.log(this)
+// }
+// abc()//Returns window object
+//console.log(obj)
+
