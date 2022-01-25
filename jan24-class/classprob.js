46e94c34d6f25bdf8658f2aa7556551cf6cbd506
Suvarchala30 <suvarchalareddy1998@gmail.com>
2022-01-25 16:08:11 +0530

    jan25

---
+++ jan24-class/classprob.js
@@ -0,0 +1,11 @@
+
+let bankbal=[30000,10000,50000,20000,40000]
+//return an arry with updated bank balances in such a fashion : if first difit is even give 20% bonus, if odd give 35% bonus
+// let arr=bankbal.map((value,index)=>{
+//     return value
+    
+// })
+// console.log(arr)
+let arr={...bankbal}
+
+console.log(arr)
