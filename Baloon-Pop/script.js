db5a0a5b407c96324cb8a26e42718b07c98e52b0
Suvarchala30 <suvarchalareddy1998@gmail.com>
2022-01-29 13:01:30 +0530

    baloon pop

--- Baloon-Pop/script.js
+++ Baloon-Pop/script.js
@@ -1,2 +1,35 @@
 const container=document.querySelector(".ballon-container")
-const zeroBalloon=document.querySelector(".zero-ballon")
+let popped=0
+for(let i=1;i<=25;i++){
+   const divEl= document.createElement("div")
+    divEl.classList.add("balloon")
+    divEl.innerText="POP!"
+    const randColor=getColor()
+    divEl.style.backgroundColor=`${randColor}`
+    divEl.style.color=`${randColor}`
+    container.appendChild(divEl)
+    divEl.addEventListener("mouseenter",function (){
+        if(divEl.style.backgroundColor!=="transparent"){
+            popped++
+        }
+        divEl.style.backgroundColor="transparent"
+        checkAllPopped()
+    })
+
+}
+function checkAllPopped(){
+    if(popped==25){
+
+let zeroBalloon=document.querySelector(".zero-balloon")
+        console.log("All are popped")
+        zeroBalloon.style.display="block"
+        container.innerHTML=''
+    }
+}
+function getColor(){
+    return `rgb(${random(255)},${random(255)},${random(255)})`
+}
+function random(number){
+    let val=Math.floor(Math.random()*number)
+    return val
+}
