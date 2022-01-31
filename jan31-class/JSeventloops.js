cf71597033ea0f3acf2b06f6701ee4deaf612105
Suvarchala30 <suvarchalareddy1998@gmail.com>
2022-01-31 23:09:26 +0530

    31st

---
+++ jan31-class/JSeventloops.js
@@ -0,0 +1,12 @@
+console.log(1)
+console.log(2)
+console.log(3)
+
+setTimeout(function(){
+    console.log('four')
+},0)
+setTimeout(function(){
+console.log(4)
+},0)
+console.log(5)
+console.log(6)
