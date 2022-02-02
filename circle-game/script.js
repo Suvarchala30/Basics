cc48b5277b3fd48dbf90cbbfedf1291e0496f54b
Suvarchala30 <suvarchalareddy1998@gmail.com>
2022-02-02 17:07:23 +0530

    circle-game

--- circle-game/script.js
+++ circle-game/script.js
@@ -1,45 +1,123 @@
-const directions = document.querySelector(".directions")
-const message = document.querySelector(".message")
-const button = document.querySelector("button")
-const gameArea = document.querySelector(".gameArea")
-const results = document.querySelector(".results")
+const message = document.querySelector(".message");
+const button = document.querySelector("button");
+const gameArea = document.querySelector(".gameArea");
+const results = document.querySelector(".results");
+const directions = document.querySelector(".directions");
 
 let inPlay = false;
 let count = 0;
-let playArea  = {
-
-}
+let playArea = {};
 
 function showMessage(notification){
-    message.innerHTML = `<h3>${notification}</h3>`
+    message.innerHTML=`<h3>${notification}</h3>`
 }
-
 
 function random(number){
-    let val = Math.floor(Math.random()*number)
+    return Math.floor(Math.random()*number)
+}
 
-    return val;
+function getColor(){
+    function color(){
+        let hex=random(255).toString(16)
+        return hex.padStart(2,"0")
+    }
+    return "#"+color()+color()+color()
+}
+
+
+function myBox(){
+    let element=document.createElement("div");
+    element.classList.add("box");
+    element.style.top=random(getTopHeight())+"px"
+   element.style.left=random(getLeftWidth())+"px"
+   element.style.backgroundColor=getColor();
+   element.start=new Date().getTime();
+   element.addEventListener("click",hit);
+
+   playArea.tempTimer=setTimeout(
+       () => hit({ target: {start: element.start } }),
+       2000
+   );
+    
+
+    gameArea.appendChild(element);
 }
 
 
 
-showMessage("Click Start to Begin!")
+function hit(e){
+    let start=e.target.start;
+    let end=new Date().getTime();
 
-button.addEventListener("click",function(){
+    let duration=(end-start)/1000;
 
-    inPlay = true;
+    if(playArea.tempTimer){
+        clearTimeout(playArea.tempTimer);
+    }
+    let maxDuration=2
+    clearTimeout(playArea.timer);
+    showMessage(`It took you ${duration} seconds to click`);
+    if(duration > maxDuration){
+        gameArea.children[0].remove();
+        results.innerHTML=`Too slow <span id="loser"> You Lost!</span>
+        Your score was ${count}.<br> Click the start button to play again!`;
 
-    button.style.display = "none"
-    directions.style.display = "none";
-    results.innerHTML = ''
-    count = 0;
+        resetGame();
+    }else{
+        gameArea.children[0].remove();
+        playArea.timer=setTimeout(myBox,random(4000));
+        count++;
+        if(count===15){
+            results.innerHTML=`You reached ${count}! <span id="winner"> You win!</span>
+            <br> Click the start button to play again!`;
+            resetGame();
+        }else{
+            results.innerHTML=`Score: ${count} of 15`
+        }
 
-    showMessage("Starting...");
-    showBox();
-})
+    }
+
+}
+
+function resetGame(){
+    clearTimeout(playArea.timer);
+    inPlay=false;
+    button.style.display="block"
+}
+
+function getTopHeight(){
+    let maxheight=gameArea.clientHeight
+    if(maxheight<=100)
+    maxheight+=200
+    else
+    maxheight-=200
+
+    return maxheight
+}
+
+function getLeftWidth(){
+    let maxwidth=gameArea.clientWidth
+    if(maxwidth<=100)
+    maxwidth+=200
+    else
+    maxwidth-=200
+
+    return maxwidth
+}
 
 
 function showBox(){
-    playArea.timer = "Testing";
+    playArea.timer=setTimeout(myBox,random(4000))
 }
-console.log(playArea);
+
+showMessage("Click start to Begin!")
+
+button.addEventListener("click",function(){
+    inPlay=true
+    directions.style.display="none"
+    button.style.display="none"
+    results.innerHTML=""
+    count=0
+    showMessage("Starting...")
+    showBox()
+});
