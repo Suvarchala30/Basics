1b1dc1aa84488ede63899398c91925487a49ed6f
Suvarchala30 <suvarchalareddy1998@gmail.com>
2022-02-12 15:51:47 +0530

    prob

--- jan26-class/classprob.js
+++ jan26-class/classprob.js
@@ -1,11 +1,7 @@
 
 //Given an array of n elements,elements can be anywhere from 0 to n, starting from 0 , find out the element which is missing in the array
 let arr=[1,6,4,3,0]
-let total=0
-let y=(arr,n)=>{
-    total=(n+1)*(n)/2
-}
-y(arr,arr.length-1)
+let total=(arr.length)*(arr.length -1)/2
 console.log(total)
 arr.map((value)=>{
     if(value<arr.length){
