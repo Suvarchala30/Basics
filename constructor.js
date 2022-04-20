b582b50cc4207a3964c3eb63ee9fdec12aa7ac2d
Suvarchala30 <suvarchalareddy1998@gmail.com>
2022-04-20 20:08:11 +0530

    cons

---
+++ constructor.js
@@ -0,0 +1,9 @@
+class student{
+    constructor(id,name,subj){
+        this.id=id
+        this.name=name
+        this.subj=subj
+    }
+}
+let suvarchala = new student(1,"Suvarchala","English")
+console.log(suvarchala)
